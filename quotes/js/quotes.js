ec8d00f9fbce0f97ed0e8dab2ab5aef185cca3b1
Matthew Sprague <spragumc@clarkson.edu>
2022-01-19 22:30:20 -0500

    Updated quotes, margins

--- quotes/js/quotes.js
+++ quotes/js/quotes.js
@@ -1,15 +1,27 @@
 const quotes = [
     {
-        "text": "Placeholder 1",
-        "name": "Me"
+        "text": "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        "name": "Michael Scott"
     },
     {
-        "text": "Placeholder 2",
-        "name": "Also me"
+        "text": "Do it. No cop, no stop.",
+        "name": "Nico De Lucia, my friend :)"
     },
     {
-        "text": "Placeholder 3",
-        "name": "Still me"
+        "text": "I have the simplest tastes. I am always satisfied with the best.",
+        "name": "Oscar Wilde"
+    },
+    {
+        "text": "Where there is no struggle, there is no strength.",
+        "name": "Oprah Winfrey"
+    },
+    {
+        "text": "If you try to make such projects, unseen by others, as perfect as any human could, you'll develop skills that other professionals don't have.",
+        "name": "Steve Wozniak"
+    },
+    {
+        "text": "I don’t talk about these things if I haven’t lived them, and I’ve hurt people in my life. It’s something I still have to think about when I sleep at night. The message I’m sending to myself – I can’t change the world until I change myself first.",
+        "name": "Kendrick Lamar Duckworth"
     }
 ]
 
